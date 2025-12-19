4213867ab8cea9fa6b9be0f04fa39a0b90e162c2
vaultsandbox-dev <vaultsandbox@gmail.com>
2025-12-19 23:51:08 +0000

    astro config trailingSlash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://vaultsandbox.dev',
+	trailingSlash: 'always',
 	integrations: [
 		starlight({
 			title: 'VaultSandbox',
