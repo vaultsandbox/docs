672e61813f9d123fc31430af78d2ff244f44e0cb
vaultsandbox-dev <vaultsandbox@gmail.com>
2026-01-11 16:28:27 +0000

    version update

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -2,11 +2,11 @@
 // Update these values when releasing new versions
 
 export const versions = {
-	dotnet: '0.6.0',
-	java: '0.6.0',
-	go: '0.6.0',
-	node: '0.6.0',
-	python: '0.6.0',
+	dotnet: '0.6.1',
+	java: '0.6.1',
+	go: '0.6.1',
+	node: '0.6.1',
+	python: '0.6.1',
 } as const;
 
 export type ClientName = keyof typeof versions;
